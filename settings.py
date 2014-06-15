58a1599bf62d82be24e5a74316ba96d981c2cd1e
Eugene Belozerov <BelozJA@gmail.com>
2014-06-16 03:46:35 +0400

    fix static

--- settings.py
+++ settings.py
@@ -82,10 +82,11 @@ CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+STATIC_ROOT = "staticfiles"
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'assets'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 MEDIA_URL = STATIC_URL + "media/"
