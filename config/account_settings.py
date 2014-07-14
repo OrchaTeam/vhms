0a3f9abea714d2514db830e454c3037d30427e9f
Eugene Belozerov <BelozJA@gmail.com>
2014-07-15 00:13:31 +0400

    adding account_settings.py

---
+++ config/account_settings.py
@@ -0,0 +1,32 @@
+LOGIN_URL = '/accounts/login/'
+
+LOGOUT_URL = '/accounts/logout/'
+
+LOGIN_REDIRECT_URL = '/'
+
+ACCOUNT_AUTHENTICATION_METHOD = "email"
+
+ACCOUNT_EMAIL_REQUIRED = True
+
+ACCOUNT_EMAIL_VERIFICATION = ("optional")
+
+SOCIALACCOUNT_QUERY_EMAIL = False
+
+SOCIALACCOUNT_EMAIL_REQUIRED = False
+
+SOCIALACCOUNT_EMAIL_VERIFICATION = ("none")
+
+#Email confirmation settings
+EMAIL_USE_TLS = True
+
+SERVER_EMAIL = 'orcha@user.z8.ru'
+
+DEFAULT_FROM_EMAIL = 'orcha@user.z8.ru'
+
+EMAIL_HOST = 'smtp.peterhost.ru'
+
+EMAIL_PORT = 587
+
+EMAIL_HOST_USER = 'orcha_00'
+
+EMAIL_HOST_PASSWORD = '9D3kIExyi5V'
