c8340d63dc572dc607685e58deecd331d6d98664
Eugene Belozerov <BelozJA@gmail.com>
2014-07-25 01:10:06 +0400

    fix profile link

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -3,5 +3,5 @@ from mezzanine.accounts import views
 #from .views import profile_view
 
 urlpatterns = patterns('',
-    url(r'^profile/(?P<username>.*)/$', views.profile, {'template': 'profiles/profiles_profile.html'}, name="profiles_profile"),
+    url(r'^profile/(?P<username>.*)/$', views.profile, {'template': 'profiles/profiles_profile.html'}, name="profile"),
 )
