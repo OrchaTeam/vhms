623b547cc09e6107d3f475abca5b756010a63b24
Eugene Belozerov <BelozJA@gmail.com>
2014-07-25 02:01:57 +0400

    add template profiles_update. fix links to the profile and update templates

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -3,5 +3,6 @@ from mezzanine.accounts import views
 #from .views import profile_view
 
 urlpatterns = patterns('',
-    url(r'^profile/(?P<username>.*)/$', views.profile, {'template': 'profiles/profiles_profile.html'}, name="profile"),
+    url(r'^profile/page/(?P<username>.*)/$', views.profile, {'template': 'profiles/profiles_profile.html'}, name="profile"),
+    url(r'^profile/update/$', views.profile_update, {'template': 'profiles/profiles_update.html'}, name="profiles_update"),
 )
