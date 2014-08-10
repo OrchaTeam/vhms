aea3448e8a23b9407fb4ec2204dbd349d7f1f496
Eugene Belozerov <BelozJA@gmail.com>
2014-08-10 20:15:28 +0400

    overrided password reset view and template

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     url(r"^accounts/login/$", 'mezzanine.accounts.views.login', name="accounts_login"),
     url(r"^accounts/logout/$", 'mezzanine.accounts.views.logout', name="accounts_logout"),
     url(r"^accounts/signup/$", 'mezzanine.accounts.views.signup', name="accounts_signup"),
+    url(r"^accounts/reset/$", 'mezzanine.accounts.views.password_reset', name="accounts_reset"),
 )
