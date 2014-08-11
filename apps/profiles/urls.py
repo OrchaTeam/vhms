9cd7c6eb2c08a9575812413faf6573c68df771e7
Eugene.Belozerov <BelozJA@gmail.com>
2014-08-11 23:15:01 +0400

    change profile

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -5,4 +5,5 @@ from mezzanine.accounts import views
 urlpatterns = patterns('',
     url(r'^profile/page/(?P<username>.*)/$', views.profile, {'template': 'profiles/profiles_profile.html'}, name="profile"),
     url(r'^profile/update/$', views.profile_update, {'template': 'profiles/profiles_update.html'}, name="profiles_update"),
+    url(r'^profile/account/$', views.profile_update, {'template': 'profiles/profiles_account.html'}, name="profiles_account"),
 )
