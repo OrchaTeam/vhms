debb5bddb8d6c2ad1d6dfde1cb6d0b803daa69f6
Eugene Belozerov <BelozJA@gmail.com>
2014-10-05 23:16:29 +0400

    added s3 settings

---
+++ config/storages_settings.py
@@ -0,0 +1,11 @@
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+
+AWS_S3_SECURE_URLS = False       # use http instead of https
+
+AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related query parameters for requests
+
+AWS_S3_ACCESS_KEY_ID = "AKIAJ3DUP27FNH7MPWIQ "     # enter your access key id
+
+AWS_S3_SECRET_ACCESS_KEY = "2dDjZuPUSxvmt+MeQW540iAuvSTwfTZc3EjIC+RW " # enter your secret access key
+
+AWS_STORAGE_BUCKET_NAME = 'vhms-media'
