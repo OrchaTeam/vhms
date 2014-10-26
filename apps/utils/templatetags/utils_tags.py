a12dea3a772deffaacc66710faa910e1be6e440d
Eugene Belozerov <BelozJA@gmail.com>
2014-10-26 20:19:04 +0400

    added  avatar tag

--- apps/utils/templatetags/utils_tags.py
+++ apps/utils/templatetags/utils_tags.py
@@ -4,10 +4,20 @@ from apps.utils import utils
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True, name='active')
 def active(context, view):
     """
     It returns the 'active' string for a current and reverse url path.
     """
-    request = context['request']
-    return utils.ActiveMenu(request, view).is_active
+    return utils.VHMSActiveMenu(context['request'], view).is_active
+
+@register.simple_tag(takes_context=False, name='avatar')
+def avatar(value, size=None):
+    """
+    It returns a view for avatar. size values: 'full', 'thumb'.
+    full is original image; thumb is thumbnail
+    :param value of avatar:
+    :return: string for a view of avatar
+    """
+    return utils.VHMSProfileAvatarField(value, size).avatar
--- apps/utils/utils.py
+++ apps/utils/utils.py
@@ -1,9 +1,11 @@
 from django.core.urlresolvers import reverse, NoReverseMatch
+import settings
 
-class ActiveMenu(object):
+class VHMSActiveMenu(object):
     """
     It provides a view of an active tab for the menu.
     """
+
     def __init__(self, request, view):
 
         self.is_active = ""
@@ -12,3 +14,18 @@ class ActiveMenu(object):
                 self.is_active = "active"
         except NoReverseMatch:
             pass
+
+class VHMSProfileAvatarField(object):
+    """
+    It provides a view for avatar.
+    full is original avatar. thumb is a thumbnail
+    """
+
+    def __init__(self, value, size):
+        if size == 'thumb':
+            pass
+        else:
+            if value == "":
+                self.avatar = '{% static "img/default-avatar.jpeg" %}'
+            else:
+                self.avatar = '%s%s'% (settings.MEDIA_URL, value)
